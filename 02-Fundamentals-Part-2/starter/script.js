fd66dba9396ee6ebb9b3554def3c9186e2437325
Benjamin Willet <125255609+BenWillet@users.noreply.github.com>
2024-02-17 15:21:58 +0100

    fruit

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -4,10 +4,28 @@ function logger(x) {
   console.log(x);
 }
 
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-  return juice;
+// Arrow function
+const calcAge3 = (birthyear) => 2032 - birthyear;
+const age3 = calcAge3(1921);
+logger(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2024 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+};
+console.log(yearsUntilRetirement(1991, "Ben"));
+
+// Functions calling other functions
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
 }
 
-const appleJuice = fruitProcessor(5, 11);
-logger(appleJuice);
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} applepieces and ${orangePieces} orangepieces`;
+  return juice;
+}
+console.log(fruitProcessor(8, 12));
